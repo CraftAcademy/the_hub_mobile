5c15f4f9e8cdcd7d87a14da5f41dd4d8fa341d28
Laura Reale <laaru1988@gmail.com>
2019-03-26 22:29:10 +0100

    fix indentation

--- Screens/Home/HomeScreen.js
+++ Screens/Home/HomeScreen.js
@@ -1,54 +1,54 @@
 import React from "react";
-import {GetArticles} from "../../Services/ArticlesApiService";
-import {StyleSheet, Text, View, FlatList} from "react-native";
-import {Image} from "react-native-elements";
+import { GetArticles } from "../../Services/ArticlesApiService";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import { Image } from "react-native-elements";
 
 export default class HomeScreen extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            articles: []
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: []
+    };
+  }
 
-    componentDidMount() {
-        GetArticles().then(res => {
-            this.updateArticlesStateHandler(res);
-        });
-    }
+  componentDidMount() {
+    GetArticles().then(res => {
+      this.updateArticlesStateHandler(res);
+    });
+  }
 
-    updateArticlesStateHandler(articles) {
-        this.setState({
-            articles: articles
-        });
-    }
+  updateArticlesStateHandler(articles) {
+    this.setState({
+      articles: articles
+    });
+  }
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <FlatList
-                    data={this.state.articles}
-                    renderItem={this.renderArticles.bind(this)}
-                    keyExtractor={item => item.id.toString()}
-                />
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.articles}
+          renderItem={this.renderArticles.bind(this)}
+          keyExtractor={item => item.id.toString()}
+        />
+      </View>
+    );
+  }
 
-    renderArticles({ item }) {
-        const article = item;
-        return (
-            <View>
-                <Image
-                    style={{ width: 100, height: 100 }}
-                    source={{ uri: article.image }}
-                /> 
-                <Text>{article.category}</Text>
-                <Text>{article.title}</Text>
-                <Text>{article.content}</Text>
-            </View>
-        );
-    }
+  renderArticles({ item }) {
+    const article = item;
+    return (
+      <View>
+        <Image
+          style={{ width: 100, height: 100 }}
+          source={{ uri: article.image }}
+        />
+        <Text>{article.category}</Text>
+        <Text>{article.title}</Text>
+        <Text>{article.content}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
